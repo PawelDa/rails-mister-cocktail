b2958d6165bf49cfd9434afb2f54301ae1c5d1dc
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-04-05 21:25:01 +0100

    all CRUD methods in cocktais_controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -6,4 +6,37 @@ class CocktailsController < ApplicationController
   def show
     @cocktail = Cocktail.find(params[:id])
   end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+  
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.update(cocktail_params)
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def cocktail_params
+  
+    params.require(:cocktail).permit(:name)
+  end
 end
