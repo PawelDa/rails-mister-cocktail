4e8766b126d53d2112254daacd429ba72f76f26f
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-04-05 20:20:12 +0100

    added coctails to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,22 @@ json["drinks"].each do |ingredient|
   created_ingredient = Ingredient.create(name: ingredient["strIngredient1"])
   puts "Ingredient #{created_ingredient.id}: #{created_ingredient.name} created!"
 end
+puts "All ingredients were created!"
+sleep(1)
+puts "Let's create some cocktails ;)"
+sleep(1)
+old_fashioned = Cocktail.create(name: 'Old Fashioned')
+puts "#{old_fashioned.name} created!"
+esspressp_martini = Cocktail.create(name: 'Espresso martini')
+puts "#{esspressp_martini.name} created!"
+mojito = Cocktail.create(name: 'Mojito')
+puts "#{mojito.name} created!"
+margarita = Cocktail.create(name: 'Margarita')
+puts "#{margarita.name} created!"
+aperol_spritz = Cocktail.create(name: 'Aperol spritz')
+puts "#{aperol_spritz.name} created!"
+sex_on_the_beach = Cocktail.create(name: 'Sex on the beach')
+puts "#{sex_on_the_beach.name} created!"
+vodka_martini = Cocktail.create(name: 'Vodka martini')
+puts "#{vodka_martini.name} created!"
+puts 'Database seeded!'
