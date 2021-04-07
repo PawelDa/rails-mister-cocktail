6e37f9ed50526b45c1b6011eae798adcbe9f6453
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-04-07 22:33:02 +0100

    dose controller

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,15 @@
 class DosesController < ApplicationController
+  def new
+
+  end
+    
+  def create
+
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
