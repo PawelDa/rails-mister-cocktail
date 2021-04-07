0534e05a5eb0f76b41fa3d008dfdfbeb3c3071e1
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-04-07 22:47:54 +0100

    create doses methods

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,18 @@
 class DosesController < ApplicationController
   def new
-
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
   end
     
   def create
-
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'cocktails/show'
+    end
   end
 
   private
