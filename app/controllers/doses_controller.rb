51a71992ecd4a5309dbca642ad6ad9499fa4a2d6
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-04-09 20:47:22 +0100

    added destroy action to doses controller

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,5 @@
 class DosesController < ApplicationController
-  # def new
-  #   @cocktail = Cocktail.find(params[:cocktail_id])
-  #   @dose = Dose.new(dose_params)
-  #   @dose.cocktail = @cocktail
-  # end
-    
+
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
@@ -14,6 +9,12 @@ class DosesController < ApplicationController
     else
       render 'cocktails/show'
     end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
