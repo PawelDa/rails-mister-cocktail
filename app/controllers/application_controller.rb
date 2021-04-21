10c018bce412e0a4e0c7e100c76f6edce78ceb37
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-04-21 23:13:52 +0100

    added draw cocktail button to navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :get_time, :
+  before_action :get_time, :draw_cocktail
   def get_time
     t = Time.now
     @time = "#{t.day}/#{t.month}/#{t.year} #{t.hour}:#{t.min}"
   end
 
-  def draw_a_cocktail
-    
+  def draw_cocktail
+    @drawn_cocktail = Cocktail.all.shuffle.first
   end
 end
