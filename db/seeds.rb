62108149a16e35dbd26e6b9207e27559156eafce
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-04-24 17:30:16 +0100

    old fashioned added to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,22 +15,37 @@ json["drinks"].each do |ingredient|
   created_ingredient = Ingredient.create(name: ingredient["strIngredient1"])
   puts "Ingredient #{created_ingredient.id}: #{created_ingredient.name} created!"
 end
+created_ingredient = Ingredient.create(name: "Ice")
+puts "Ingredient #{created_ingredient.id}: #{created_ingredient.name} created!"
 puts "All ingredients were created!"
 sleep(1)
 puts "Let's create some cocktails ;)"
 sleep(1)
 old_fashioned = Cocktail.create(name: 'Old Fashioned')
+old_fashioned.photo.attach(io: File.open('app/assets/images/old_fashioned.jpg'), filename: "old_fashioned.jpg", content_type: 'image/jpg')
+Dose.create(description: "2 ounces", ingredient_id: 20, cocktail_id: old_fashioned.id)
+Dose.create(description: "2 ounces", ingredient_id: 49, cocktail_id: old_fashioned.id)
+Dose.create(description: "3 dashes", ingredient_id: 11, cocktail_id: old_fashioned.id)
+Dose.create(description: "cube", ingredient_id: 101, cocktail_id: old_fashioned.id)
+Dose.create(description: "pell", ingredient_id: 73, cocktail_id: old_fashioned.id)
 puts "#{old_fashioned.name} created!"
+
 esspressp_martini = Cocktail.create(name: 'Espresso martini')
 puts "#{esspressp_martini.name} created!"
+
 mojito = Cocktail.create(name: 'Mojito')
 puts "#{mojito.name} created!"
+
 margarita = Cocktail.create(name: 'Margarita')
 puts "#{margarita.name} created!"
+
 aperol_spritz = Cocktail.create(name: 'Aperol spritz')
 puts "#{aperol_spritz.name} created!"
+
 sex_on_the_beach = Cocktail.create(name: 'Sex on the beach')
 puts "#{sex_on_the_beach.name} created!"
+
 vodka_martini = Cocktail.create(name: 'Vodka martini')
 puts "#{vodka_martini.name} created!"
+
 puts 'Database seeded!'
